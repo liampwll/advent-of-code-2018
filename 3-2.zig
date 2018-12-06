cddb9258ee3ed22290aef8c6aca7dc9cc627505f
Liam Powell <liam@liampwll.com>
2018-12-06 23:42:49 +0800

    rewrite day 3

--- 3-2.zig
+++ 3-2.zig
@@ -1,20 +1,30 @@
 const std = @import("std");
-const io = std.io;
-const fmt = std.fmt;
+
+const max_x = 2000;
+const max_y = 2000;
+const max_id: u16 = 2000;
 
 pub fn main() !void {
-    var fabric = [][2000]u16{[]u16{0} ** 2000} ** 2000;
-    var valid_claims = []bool{false} ** 2000;
+    std.debug.warn("{}\n", try findValid(@embedFile("input/3")));
+}
 
-    var line_buf: [50]u8 = undefined;
-    while (io.readLine(line_buf[0..])) |line_len| {
-        var iter = std.mem.split(line_buf[0..line_len], "# @,:x");
-        const id = try fmt.parseInt(u16, iter.next().?, 10);
-        const start_x = try fmt.parseInt(u32, iter.next().?, 10);
-        const start_y = try fmt.parseInt(u32, iter.next().?, 10);
-        const size_x = try fmt.parseInt(u32, iter.next().?, 10);
-        const size_y = try fmt.parseInt(u32, iter.next().?, 10);
+fn findValid(input: []const u8) !u16 {
+    var n: u32 = 0;
+    var fabric = [][max_x]u16{[]u16{0} ** max_x} ** max_y;
+    var valid_claims = []bool{false} ** max_id;
+
+    var line_it = std.mem.split(input, "\n");
+    while (line_it.next()) |line| {
+        var num_it = std.mem.split(line, "# @,:x");
+
+        const id = try std.fmt.parseInt(u16, num_it.next().?, 10);
+        const start_x = try std.fmt.parseInt(u32, num_it.next().?, 10);
+        const start_y = try std.fmt.parseInt(u32, num_it.next().?, 10);
+        const size_x = try std.fmt.parseInt(u32, num_it.next().?, 10);
+        const size_y = try std.fmt.parseInt(u32, num_it.next().?, 10);
+
         valid_claims[id] = true;
+
         var y = start_y;
         while (y < start_y + size_y) : (y += 1) {
             var x = start_x;
@@ -26,12 +36,22 @@ pub fn main() !void {
                 fabric[y][x] = id;
             }
         }
-    } else |err| {
     }
 
     for (valid_claims) |is_valid, id| {
         if (is_valid) {
-            std.debug.warn("{}\n", id);
+            return @intCast(u16, id);
         }
     }
+
+    unreachable;
+}
+
+test "samples" {
+    const input =
+        \\#1 @ 1,3: 4x4
+        \\#2 @ 3,1: 4x4
+        \\#3 @ 5,5: 2x2
+    ;
+    std.debug.assert((try findValid(input)) == 3);
 }
--- 3.zig
+++ 3.zig
@@ -1,31 +1,46 @@
 const std = @import("std");
-const io = std.io;
-const fmt = std.fmt;
+
+const max_x = 2000;
+const max_y = 2000;
 
 pub fn main() !void {
-    var overlap: u32 = 0;
-    var fabric = [][2000]u8{[]u8{0} ** 2000} ** 2000;
+    std.debug.warn("{}\n", try overlap(@embedFile("input/3")));
+}
 
-    var line_buf: [50]u8 = undefined;
-    while (io.readLine(line_buf[0..])) |line_len| {
-        var iter = std.mem.split(line_buf[0..line_len], "# @,:x");
-        const id = try fmt.parseInt(u32, iter.next().?, 10);
-        const start_x = try fmt.parseInt(u32, iter.next().?, 10);
-        const start_y = try fmt.parseInt(u32, iter.next().?, 10);
-        const size_x = try fmt.parseInt(u32, iter.next().?, 10);
-        const size_y = try fmt.parseInt(u32, iter.next().?, 10);
+fn overlap(input: []const u8) !u32 {
+    var n: u32 = 0;
+    var fabric = [][max_x]u16{[]u16{0} ** max_x} ** max_y;
+
+    var line_it = std.mem.split(input, "\n");
+    while (line_it.next()) |line| {
+        var num_it = std.mem.split(line, "# @,:x");
+
+        const id = try std.fmt.parseInt(u32, num_it.next().?, 10);
+        const start_x = try std.fmt.parseInt(u32, num_it.next().?, 10);
+        const start_y = try std.fmt.parseInt(u32, num_it.next().?, 10);
+        const size_x = try std.fmt.parseInt(u32, num_it.next().?, 10);
+        const size_y = try std.fmt.parseInt(u32, num_it.next().?, 10);
+
         var y = start_y;
         while (y < start_y + size_y) : (y += 1) {
             var x = start_x;
             while (x < start_x + size_x) : (x += 1) {
                 if (fabric[y][x] == 1) {
-                    overlap += 1;
+                    n += 1;
                 }
                 fabric[y][x] += 1;
             }
         }
-    } else |err| {
     }
 
-    std.debug.warn("{}\n", overlap);
+    return n;
+}
+
+test "samples" {
+    const input =
+        \\#1 @ 1,3: 4x4
+        \\#2 @ 3,1: 4x4
+        \\#3 @ 5,5: 2x2
+    ;
+    std.debug.assert((try overlap(input)) == 4);
 }
