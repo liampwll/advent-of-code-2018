816c67973dc2f0e5e73f06c8050f2493ae07bdf4
Liam Powell <liam@liampwll.com>
2018-12-14 19:37:56 +0800

    cleanup 11

--- 11.zig
+++ 11.zig
@@ -1,14 +1,11 @@
 const std = @import("std");
 
 pub fn main() void {
-    var direct_allocator = std.heap.DirectAllocator.init();
-    defer direct_allocator.deinit();
-
-    var res = something(2187);
+    var res = maxPower(2187);
     std.debug.warn("{},{}\n", res[0], res[1]);
 }
 
-fn something(serial: i32) [2]i32 {
+fn maxPower(serial: i32) [2]i32 {
     var high: i32 = -2000000000;
     var high_x: i32 = 0;
     var high_y: i32 = 0;
@@ -17,15 +14,14 @@ fn something(serial: i32) [2]i32 {
     while (y < 298) : (y += 1) {
         var x: i32 = 1;
         while (x < 298) : (x += 1) {
-            var sum = power(x + 0, y + 0, serial)
-                + power(x + 1, y + 0, serial)
-                + power(x + 2, y + 0, serial)
-                + power(x + 0, y + 1, serial)
-                + power(x + 1, y + 1, serial)
-                + power(x + 2, y + 1, serial)
-                + power(x + 0, y + 2, serial)
-                + power(x + 1, y + 2, serial)
-                + power(x + 2, y + 2, serial);
+            var sum: i32 = 0;
+            var dy: i32 = 0;
+            while (dy < 3) : (dy += 1) {
+                var dx: i32 = 0;
+                while (dx < 3) : (dx += 1) {
+                    sum += power(x + dx, y + dy, serial);
+                }
+            }
             if (sum > high) {
                 high = sum;
                 high_x = x;
