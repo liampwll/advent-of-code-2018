a4e0bb87025fcb3c254ce9c5783172115577a392
Liam Powell <liam@liampwll.com>
2018-12-18 14:29:22 +0800

    Rewrite 18-2

--- 18-2.zig
+++ 18-2.zig
@@ -1,9 +1,82 @@
 const std = @import("std");
 
 const SIZE = 50;
+const STEPS = 1000000000;
 
-pub fn main() void {
-    lumber(@embedFile("input/18"));
+pub fn main() !void {
+    var direct_allocator = std.heap.DirectAllocator.init();
+    defer direct_allocator.deinit();
+
+    std.debug.warn("{}\n", try lumber(&direct_allocator.allocator, @embedFile("input/18")));
+}
+
+fn lumber(allocator: *std.mem.Allocator, input: []const u8) !u32 {
+    var area: [SIZE][SIZE]u8 = undefined;
+    var seen = std.hash_map.HashMap([SIZE][SIZE]u8, u32, fnv1a32, arrayEql).init(allocator);
+
+    var line_it = std.mem.split(input, "\n");
+    {var y: usize = 0; while (line_it.next()) |line| {
+        for (line) |char, x| {
+            area[y][x] = char;
+        }
+        y += 1;
+    }}
+
+
+    var i: u32 = 0;
+    const prev = while (i < STEPS) : (i += 1) {
+        const gop = try seen.getOrPut(area);
+        if (gop.found_existing) {
+            break gop.kv.value;
+        } else {
+            gop.kv.value = i;
+        }
+
+        step(&area);
+    } else unreachable;
+
+    const steps_left = (STEPS - i) % (i - prev);
+
+    var j: u32 = 0;
+    while (j < steps_left) : (j += 1) {
+        step(&area);
+    }
+
+    var trees: u32 = 0;
+    var yards: u32 = 0;
+    for (area) |line| {
+        for (line) |char| {
+            if (char == '|') {
+                trees += 1;
+            } else if (char == '#') {
+                yards += 1;
+            }
+        }
+    }
+
+    return trees * yards;
+}
+
+fn step(area: *[SIZE][SIZE]u8) void {
+    const area_copy = area.*;
+    for (area_copy) |line, y| {
+        for (line) |char, x| {
+            if (char == '.') {
+                if (countAdjacent(area_copy, y, x, '|') >= 3) {
+                    area[y][x] = '|';
+                }
+            } else if (char == '|') {
+                if (countAdjacent(area_copy, y, x, '#') >= 3) {
+                    area[y][x] = '#';
+                }
+            } else if (char == '#') {
+                if (countAdjacent(area_copy, y, x, '#') == 0
+                        or countAdjacent(area_copy, y, x, '|') == 0) {
+                    area[y][x] = '.';
+                }
+            }
+        }
+    }
 }
 
 fn countAdjacent(area: [SIZE][SIZE]u8, y: usize, x: usize, char: u8) u32 {
@@ -38,87 +111,23 @@ fn countAdjacent(area: [SIZE][SIZE]u8, y: usize, x: usize, char: u8) u32 {
     return count;
 }
 
-fn lumber(input: []const u8) void {
-    var area: [SIZE][SIZE]u8 = undefined;
-
-    var line_it = std.mem.split(input, "\n");
-    {var y: usize = 0; while (line_it.next()) |line| {
+fn arrayEql(key_a: [SIZE][SIZE]u8, key_b: [SIZE][SIZE]u8) bool {
+    for (key_a) |line, y| {
         for (line) |char, x| {
-            area[y][x] = char;
-        }
-        y += 1;
-    }}
-
-    {var i: u32 = 0; while (i < 1000000000) : (i += 1) {
-        const area_copy = area;
-        for (area_copy) |line, y| {
-            for (line) |char, x| {
-                if (char == '.') {
-                    if (countAdjacent(area_copy, y, x, '|') >= 3) {
-                        area[y][x] = '|';
-                    }
-                } else if (char == '|') {
-                    if (countAdjacent(area_copy, y, x, '#') >= 3) {
-                        area[y][x] = '#';
-                    }
-                } else if (char == '#') {
-                    if (countAdjacent(area_copy, y, x, '#') == 0
-                            or countAdjacent(area_copy, y, x, '|') == 0) {
-                        area[y][x] = '.';
-                    }
-                }
+            if (key_b[y][x] != char) {
+                return false;
             }
         }
-
-        var trees: u32 = 0;
-        var yards: u32 = 0;
-        for (area) |line| {
-            for (line) |char| {
-                if (char == '|') {
-                    trees += 1;
-                } else if (char == '#') {
-                    yards += 1;
-                }
-            }
-        }
-        std.debug.warn("{}, {}\n", i + 1, trees * yards);
-    }}
+    }
+    return true;
 }
 
-// Output:
-// ...
-// 84794, 172898
-// 84795, 169470 -----+
-// 84796, 165780      |
-// 84797, 169260      |
-// 84798, 169524      |
-// 84799, 168935      |
-// 84800, 173952      |
-// 84801, 179622      |
-// 84802, 180299      |
-// 84803, 188155      |
-// 84804, 195776      |
-// 84805, 192198      |
-// 84806, 199076      |
-// 84807, 206375      |
-// 84808, 210576      | -- 28 lines apart
-// 84809, 208658      |
-// 84810, 212520      |
-// 84811, 213395      |
-// 84812, 216660      |
-// 84813, 210368      |
-// 84814, 203794      |
-// 84815, 195510      |
-// 84816, 180873      |
-// 84817, 177126      |
-// 84818, 176320      |
-// 84819, 167862      |
-// 84820, 169218      |
-// 84821, 170428      |
-// 84822, 172898      |
-// 84823, 169470 -----+
-// ...
-//
-// (1000000000 - 84795) % 28 = 9
-// 84795 + 9 = 84804
-// Line 84804 is 195776, therefore that's the answer
+fn fnv1a32(key: [SIZE][SIZE]u8) u32 {
+    var res: u32 = 2166136261;
+    for (key) |line| {
+        for (line) |char| {
+            res = (res ^ @intCast(u32, char)) *% 16777619;
+        }
+    }
+    return res;
+}
